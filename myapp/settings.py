33bfd4a0c7db0efd0b09517dc50ed2a5cd037a49
mohamedalijas <alijasmohammed@gmail.com>
2025-03-13 12:23:37 +0530

    id checking

--- myapp/settings.py
+++ myapp/settings.py
@@ -11,9 +11,9 @@ SECRET_KEY = 'django-insecure-7_zm_h$h4$mpq+)j&(u-d!o#f&asrdy-x^eip5i8ibv1s3vt-k
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['printapp12-2.onrender.com']
+ALLOWED_HOSTS = ['piqapp.onrender.com']
 
-CSRF_TRUSTED_ORIGINS = ['https://printapp12-2.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https:piqapp.onrender.com']
 
 
 # Application definition
